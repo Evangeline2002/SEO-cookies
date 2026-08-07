cb6b1d938f4425f57e21f800b16be5311ecf7722
Evangeline2002 <evangelinejeba62@gmail.com>
2026-08-07 17:39:31 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'cookie_heaven',
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 2,
     queueLimit: 0
 });
 
